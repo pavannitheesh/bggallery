776326b4a0e04fb26fcb11894acf2cfdf9b7a1e4
pavannitheesh <gpavannitheesh1@gmail.com>
2025-05-26 01:18:41 +0530

    refactor(BackgroundGallery): improve layout and add theme support

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from 'react';
-import BackgroundGallery from '@/app/_components/BackgroundGallery';
+import{ BackgroundGallery }from '@/app/_components/BackgroundGallery';
 import { BACKGROUND_OPTIONS as backgroundsData } from '@/data/background';
 
 export default function Home() {
@@ -11,25 +11,30 @@ export default function Home() {
   };
 
   const Background = backgroundsData[activeBackground].component;
-
+  const isDarkTheme = backgroundsData[activeBackground].theme === 'dark';
   return (
-    <main className="min-h-screen w-full relative pt-[225px]">
-      {/* Active Background */}
+    <>
+    
+    <div className="fixed left-0 top-0 -z-10 h-full w-full">
     {Background}
+  </div>
+    <main className="h-screen w-full relative pt-[225px]">
       
       {/* Content */}
       <div className="relative z-10 p-6 container mx-auto">
-        <h1 className="text-4xl font-bold mb-6 text-center">Background Gallery</h1>
-        <p className="text-lg text-center mb-10 max-w-xl mx-auto">
+        <h1 className={`text-4xl font-bold mb-6 text-center ${isDarkTheme ? 'text-white' : 'text-black'}`}>Background Gallery</h1>
+        <p className={`text-lg text-center mb-10 max-w-xl mx-auto ${isDarkTheme ? 'text-white' : 'text-black'}`}>
           Click on any background to apply it to the page. 
           Use the "Copy Code" button to copy the implementation.
         </p>
-        
+          </div>
+        <div className="overflow-hidden px-4 pb-20 pt-52 md:px-10">
         <BackgroundGallery 
           activeIndex={activeBackground}
           onBackgroundChange={handleBackgroundChange}
         />
-      </div>
+        </div>
     </main>
+    </>
   );
 }
